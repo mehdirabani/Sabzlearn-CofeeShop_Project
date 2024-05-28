bbdfdb9fcb6ad6f3accadcf61837587e594d134c
mehdirabani <pgm.mehdirabani@gmail.com>
2024-05-29 00:16:49 +0330

    Creat Basic File & Document

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+      boxShadow: {
+        shadowNormal: "0 1px 10px rgba(0,0,0,5%)",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana medium",
+        DanaBold: "Dana bold",
+        Morabba: "Morabba",
+        MorabbaMedium: "Morabba medium",
+        MorabbaBold: "Morabba bold",
+      },
+    },
+  },
+  plugins: [],
+};
